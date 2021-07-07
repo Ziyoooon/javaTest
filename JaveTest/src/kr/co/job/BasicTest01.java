72744147f4aa74f206ab76464fee9e668a0bdd9d
YONSAI <YONSAI@402-15>
2021-07-07 12:42:06 +0900

    "test"

---
+++ JaveTest/src/kr/co/job/BasicTest01.java
@@ -0,0 +1,24 @@
+
+package kr.co.job;
+
+import java.util.Scanner;
+
+public class BasicTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int num = 24;
+		System.out.println((num / 10 * 10 + 10) - num);
+		
+		//3-7 문제 (문제이해x)
+		int fahrenhrit = 100;
+	//	float celcius = 5/9 x (F - 32);
+		
+//		System.out.println("Fahrenheit:"+fahrenhrit);
+//		System.out.println("Celcius:"+celcius);
+		
+		long l = 3000*3000*3000L;
+	}
+
+}
